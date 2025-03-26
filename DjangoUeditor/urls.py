0042b46e0ca74ee3ff58447c1f49c8e5704ddf04
WangHeng <18810900225@163.com>
2025-03-26 11:40:07 +0800

    222

--- DjangoUeditor/urls.py
+++ DjangoUeditor/urls.py
@@ -1,22 +1,21 @@
 # -*- coding: utf-8 -*-
 import django
 from .views import get_ueditor_controller
-
+from django.urls import  re_path
 DJANGO_VERSION = django.VERSION[:2]
 
 
 if DJANGO_VERSION >= (1, 8):
-    from django.conf.urls import url
     urlpatterns = [
-        url(r'^controller/$', get_ueditor_controller)
+        re_path(r'^controller/$', get_ueditor_controller)
     ]
 
 else:
     try:
-        from django.conf.urls import patterns, url
+        from urls import patterns, url
     except ImportError:
-        from django.conf.urls.defaults import patterns, url
+        from urls.defaults import patterns, url
 
     urlpatterns = patterns('',
-        url(r'^controller/$', get_ueditor_controller)
+                           re_path(r'^controller/$', get_ueditor_controller)
     )
