bfbf92f9f016a6b7124c15a04fb2b7057490ff91
WangHeng <18810900225@163.com>
2025-03-26 11:42:29 +0800

    222

--- ishare/settings.py
+++ ishare/settings.py
@@ -117,21 +117,30 @@ WSGI_APPLICATION = 'ishare.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     },
+#     # 'default': {
+#     #     'ENGINE': 'django.db.backends.mysql',
+#     #     'NAME': 'ishare',
+#     #     'USER': 'root',
+#     #     'PASSWORD': 'lujianxin.com',
+#     #     'HOST': 'lujianxin.com',
+#     #     'PORT': '3306',
+#     # }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'ishare',
-    #     'USER': 'root',
-    #     'PASSWORD': 'lujianxin.com',
-    #     'HOST': 'lujianxin.com',
-    #     'PORT': '3306',
-    # }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dxf',
+        'USER': 'dxf',
+        'PASSWORD': '123',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
+    }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
